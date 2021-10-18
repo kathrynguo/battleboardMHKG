1914a9569d9f3f21c889b0a6a07ea6b3c72a8618
kathrynguo <59924894+kathrynguo@users.noreply.github.com>
2021-10-18 08:13:19 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -45,6 +45,53 @@ public class Computer {
 
 		
 	}
+	
+	/*
+		public void GuessCheck()  {
+			String guess = mackenziesclass.guessing variable //haven't gotten her user input variable
+			char row = guess.charAt(0); 
+				if (row == "a" || row == "A") { 
+					int roe = 1; 
+				}
+				else if (row == "b" || row == "B") { 
+					roe = 2; 
+				}
+				else if (row == "c" || row == "C") { 
+					roe = 3; 
+				}
+				else if (row == "d" || row == "D") { 
+					roe = 4; 
+				}
+				else if (row == "e" || row == "E") { 
+					roe = 5; 
+				}
+				else if (row == "f" || row == "F") { 
+					roe = 6; 
+				}
+				else if (row == "g" || row == "G") { 
+					roe = 7; 
+				}
+				else if (row == "h" || row == "H") { 
+					roe = 8; 
+				}
+				else if (row == "i" || row == "I") { 
+					roe = 9; 
+				}
+				else { 
+					roe = 10; 
+				}
+			char column = guess.charAt(1); 
+			boolean hit; 
+			if (compBoard[roe][column] == 1) {
+			* 		hit = true;
+			* }
+			else {
+			* 		hit = false; 
+			* }
+			
+		}
+	*/
+		
 		
 } 
 
