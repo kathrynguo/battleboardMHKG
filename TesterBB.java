247a86e8ce1dcc45af86dc0cb508df4c818d27e3
kathrynguo <59924894+kathrynguo@users.noreply.github.com>
2021-10-18 09:31:40 -0400

    Update TesterBB.java

--- TesterBB.java
+++ TesterBB.java
@@ -3,7 +3,11 @@ public class TesterBB {
 	public static void main (String[] args) {
 	
 	Computer newComp = new Computer();
-	newComp.Computer();
+	newComp.printBoard(); 
+	newComp.placingShips();
+	//printing space in between boards
+	System.out.println(""); 
+	newComp.emptyBoard(); 
 	
 	}
 
