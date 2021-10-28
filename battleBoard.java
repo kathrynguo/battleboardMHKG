b69a6aaac3a673a3b212ee0ae2896bef803751cf
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2021-10-28 09:35:41 -0400

    Update battleBoard.java
    stuff i did in class

--- battleBoard.java
+++ battleBoard.java
@@ -4,90 +4,123 @@ public class battleBoard {
 	String shipP1;
 	String shipP2;
 	public void array() {
-		char c;
+		int[][] compBoard = new int[10][10];
+		//print numbers across top 
 		int a = 64;
-		int[][] numbers = new int[10][10];
-		System.out.println("   1 2 3 4 5 6 7 8 9 10");
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 0; j < numbers[0].length; j++) {
-				if (j == 0) {
-					a = a + 1;
-					c = (char)a;
-					System.out.print(c + "  ");
-				}
-				else if (j == 0 || j % (numbers.length - 1) != 0) {
-					System.out.print(numbers[i][j] + " ");
-				}
-				else {
-					System.out.println(numbers[i][j] + " ");
-				}
-			}
+		char c;
+		System.out.print("   1 2 3 4 5 6 7 8 9 10");
+		for (int i = 0; i < compBoard.length; i++) {
+			//print start of row in letters 
+			a = a + 1;
+			c = (char)a;
+			System.out.print("\n" + c + "  ");
+			for (int j = 0; j < compBoard[0].length; j++) {
+		
+					System.out.print(compBoard[i][j] + " ");
+			}//close col loop
 		}
-	} // closes array()
+	}
 
 	public void rules() {
 		System.out.println("\nRules: ");
 		System.out.println("You place your ships and the computer will place its ships then you each make guesses as t where eachother's ships are.");
-		System.out.println("You get 5 ships to place. You have 1 5 length ship, 1 4 length ship, 2 3 length ships, and 1 2 length ship.");
-		System.out.println("You will choose where to place each ship either horizontally and verticallly, no diagonally.");
+		System.out.println("You get 3 ships to place. You have a 2 length ship,  a 3 length ship, and a 4 length ship.");
+		System.out.println("You will choose where to place each ship either horizontally and verticallly, not diagonally.");
 	}
 
 	public void shipPlacementPlayer() {
-		int x = 6;
+		int x = 5;
 		while (x > 1) {
 			x = x -1;
 			Scanner keyboard = new Scanner(System.in);
-			if (x == 3) {
-				String vh = keyboard.nextLine();
-				System.out.println("\nWould you like to place thi ship vertically and horizontally:  ");
-				System.out.println("Where would you like to place your first " + x + " length ship?  Please type in the position of ships using the first and last point (Ex. A1  to A5).");
-				System.out.print("Position 1: ");
-				shipP1 = keyboard.nextLine();
-				System.out.print("\nPosition 2: ");
-				shipP2 = keyboard.nextLine();
-				System.out.println("\nWhere would you like to place your first " + x + " length ship?  Please type in the position of ships using the first and last point (Ex. A1  to A5).");
-				System.out.print("Position 1: ");
-				shipP1 = keyboard.nextLine();
-				System.out.print("\nPosition 2: ");
-				shipP2 = keyboard.nextLine();
-				
-				if (vh.equals("horizontal")) {
-					if (shipP1.equals(shipP2)) {
-						System.out.println("\nThe start and end point of the ship cannot be the same! Try again!");
+			
+			System.out.println("\nWould you like to place thi ship vertically and horizontally:  ");
+			String vh = keyboard.nextLine();
+			System.out.println("\nWhere would you like to place your " + x + " length ship?  Please type in the position of ships using the first and last point (Ex. A1  to A4).");
+			System.out.print("Position 1: ");
+			shipP1 = keyboard.nextLine();
+			System.out.print("\nPosition 2: ");
+			shipP2 = keyboard.nextLine();
+			if (vh.equals("vertical")) {
+				if (shipP1.equals(shipP2)) {
+					System.out.println("\nThe start and end point of the ship cannot be the same! Try again!");
+				}
+				char column1 = shipP1.charAt(0);
+				char column2 = shipP2.charAt(0);
+				char row1C = shipP1.charAt(1);
+				char row2C = shipP2.charAt(1);
+				int row1 = Character.getNumericValue(row1C);
+				int row2 = Character.getNumericValue(row2C);
+				if (column1 != column2) {
+					System.out.println("This ship was not placed vertically. Try Again!");
+				}
+				int checker = Math.abs(row1 - row2);
+				int pr1;
+				int pr2;
+				int pr3;
+				int pr4;
+				int pc1;
+				int pc2;
+				int pc3;
+				int pc4;
+				if (checker == 1 && x == 2) {
+					if (column1 == 'A') { 
+						pc1 = 1;
+						pc2 = 2;
+						pr1 = row1;
+						pr2 = row2;
 					}
-					char column1 = shipP1.charAt(0);
-					char column2 = shipP2.charAt(0);
-					char row1C = shipP1.charAt(1);
-					char row2C = shipP2.charAt(1);
-					int row1 = Character.getNumericValue(row1C);
-					int row2 = Character.getNumericValue(row2C);
-					else if (column1 != column2) {
-						System.out.println("This ship was not placed horizontally. Try Again!");
+				}
+				if (checker == 2 && x == 3) {
+					if (column1 == 'A') { 
+						pc1 = 1;
+						pc2 = 2;
+						pc3 = 3;
+						pr1 = row1;
+						pr1 = row1 + 1;
+						pr2 = row2;
 					}
-					int checker = Math.abs(row1 - row2);
-					else if (checker != 2) {
-						
+				}
+				if (checker == 3 && x == 4) {
+					if (column1 == 'A') { 
+						pc1 = 1;
+						pc2 = 2;
+						pc3 = 3;
+						pc4 = 4;
+						pr1 = row1;
+						pr2 = row1 + 1;
+						pr3 = row1 + 2;
+						pr4 = row2;
 					}
 				}
 			}
-			else {
-				System.out.println("\nWhere would you like to place your " + x + " length ship?  Please type in the position of ships using the first and last point (Ex. A1  to A5).");
-				System.out.print("Position 1: ");
-				shipP1 = keyboard.nextLine();
-				System.out.print("\nPosition 2: ");
-				shipP2 = keyboard.nextLine();
-				shipPlacementChecker();
+			if (vh.equals("horizontal")) {
+				if (shipP1.equals(shipP2)) {
+					System.out.println("\nThe start and end point of the ship cannot be the same! Try again!");
+				}
+				char column1C = shipP1.charAt(0);
+				char column2C = shipP2.charAt(0);
+				char row1C = shipP1.charAt(1);
+				char row2C = shipP2.charAt(1);
+				int column1 = Character.getNumericValue(column1C);
+				int column2 = Character.getNumericValue(column2C);
+				if (row1C != row1C) {
+					System.out.println("This ship was not placed prizontally. Try Again!");
+				}
+				int checker = Math.abs(column1 - column2);
+				if (checker == 1 && x == 2) {
+					
+				}
+				if (checker == 2 && x == 3) {
+						
+				}
+				if (checker == 3 && x == 4) {
+						
+				}
 			}	
 		}
 	}
-	public void shipPlacementChecker() {
-		if (shipP1.equals(shipP2)) {
-			System.out.println("The ship cannot start and end on the same point. Try again! ");
-		}
-		else if (shipP1.equals(shipP2)) {
-			System.out.println("The ship cannot start and end on the same point. Try again! ");
-		}
-	}
+	/*
 	public void shipPlacementComputer() {
 		int[][] compBoard = new int[10][10];
 	//randomly placing row
@@ -117,5 +150,6 @@ public class battleBoard {
 				}
 			System.out.println("");
 		}
-	}
+	}*/
 }
+
